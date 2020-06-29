91ae8107e18a43a7333ff4934190c8ef9307b3ce
WidgetMidget <67565373+WidgetMidget@users.noreply.github.com>
2020-06-29 03:59:33 +0200

    Update kobatochan_accesskey-navigation.user.js

--- scripts/kobatochan_accesskey-navigation.user.js
+++ scripts/kobatochan_accesskey-navigation.user.js
@@ -2,7 +2,7 @@
 // @name        [WM] Kobatochan.com Accesskey Navigation
 // @namespace   WidgetMidget
 // @description Ctrl + Arrow key navigation.
-// @version     1.3.1
+// @version     1.3.0
 // @updateURL   https://github.com/WidgetMidget/scripts-and-userstyles/raw/master/kobatochan/accesskey_navigation.user.js
 // @downloadURL https://github.com/WidgetMidget/scripts-and-userstyles/raw/master/kobatochan/accesskey_navigation.user.js
 // @supportURL  https://github.com/WidgetMidget/scripts-and-userstyles/issues
