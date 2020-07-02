a09c600f1fd573fb9b3e6755a2e4d6afa12c194c
WidgetMidget <67565373+WidgetMidget@users.noreply.github.com>
2020-07-02 21:23:04 +0200

    >:/
    Why u no show double indent in Atom yo?

--- scripts/animekisa_tv-accesskey-navigation.user.js
+++ scripts/animekisa_tv-accesskey-navigation.user.js
@@ -3,7 +3,7 @@
 // @namespace   WidgetMidget
 // @description Ctrl + Arrow key navigation.
 // @version     1.0.0
-// @updateURL		https://github.com/WidgetMidget/scripts-and-userstyles/raw/master/scripts/animekisa_tv-accesskey-navigation.user.js
+// @updateURL	https://github.com/WidgetMidget/scripts-and-userstyles/raw/master/scripts/animekisa_tv-accesskey-navigation.user.js
 // @downloadURL https://github.com/WidgetMidget/scripts-and-userstyles/raw/master/scripts/animekisa_tv-accesskey-navigation.user.js
 // @supportURL  https://github.com/WidgetMidget/scripts-and-userstyles/issues
 // @match       *://*.animekisa.tv/*
