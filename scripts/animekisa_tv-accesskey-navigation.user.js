0b9421d58d3829b7802fbd3b2a6429264fc202c1
WidgetMidget <67565373+WidgetMidget@users.noreply.github.com>
2020-07-02 21:18:10 +0200

    Added update and download URLs
    Also made my code look a little tiny bit less stupid, but that's an effort in futility.

--- scripts/animekisa_tv-accesskey-navigation.user.js
+++ scripts/animekisa_tv-accesskey-navigation.user.js
@@ -3,8 +3,8 @@
 // @namespace   WidgetMidget
 // @description Ctrl + Arrow key navigation.
 // @version     1.0.0
-// @updateURL
-// @downloadURL
+// @updateURL		https://github.com/WidgetMidget/scripts-and-userstyles/raw/master/scripts/animekisa_tv-accesskey-navigation.user.js
+// @downloadURL https://github.com/WidgetMidget/scripts-and-userstyles/raw/master/scripts/animekisa_tv-accesskey-navigation.user.js
 // @supportURL  https://github.com/WidgetMidget/scripts-and-userstyles/issues
 // @match       *://*.animekisa.tv/*
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
@@ -18,12 +18,12 @@ document.addEventListener('keydown', function(e){
 		switch (e.keyCode)
 		{
 			case 37:
-                $("#playerselector").find("option:selected").prev().prop("selected", true).trigger("change");
-            break;
+				$("#playerselector").find("option:selected").prev().prop("selected", true).trigger("change");
+      break;
 
 			case 39:
-                $("#playerselector").find("option:selected").next().prop("selected", true).trigger("change");
-            break;
+				$("#playerselector").find("option:selected").next().prop("selected", true).trigger("change");
+			break;
 		}
 	}
 }, false);
