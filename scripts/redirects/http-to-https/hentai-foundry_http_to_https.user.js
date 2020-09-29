3f7193fbb7cb8d5ff4aaa89be340296cfb367fad
WidgetMidget <67565373+WidgetMidget@users.noreply.github.com>
2020-09-29 19:54:27 +0200

    fixed derp

--- scripts/redirects/http-to-https/hentai-foundry_http_to_https.user.js
+++ scripts/redirects/http-to-https/hentai-foundry_http_to_https.user.js
@@ -2,12 +2,11 @@
 // @name         [WM] hentai-foundry 'http' to 'https' redirect
 // @namespace	 https://github.com/WidgetMidget/scripts-and-userstyles
 // @author       WidgetMidget
-// @version      2.2.8
+// @version      2.2.9
 // @icon         https://raw.githubusercontent.com/WidgetMidget/scripts-and-userstyles/master/resources/favicons/hentai-foundry.png
 // @updateURL    https://github.com/WidgetMidget/scripts-and-userstyles/raw/master/scripts/redirects/http-to-https/hentai-foundry_http_to_https.user.js
 // @downloadURL  https://github.com/WidgetMidget/scripts-and-userstyles/raw/master/scripts/redirects/http-to-https/hentai-foundry_http_to_https.user.js
 // @supportURL   https://github.com/WidgetMidget/scripts-and-userstyles/issues
-// @match        https://www.youtube.com/*
 // @match        http://www.hentai-foundry.com/*
 // ==/UserScript==
 
