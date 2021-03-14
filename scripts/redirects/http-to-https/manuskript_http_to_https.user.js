82e228f61774b26f8e3bc488561a1e31f2a51825
WidgetMidget <67565373+WidgetMidget@users.noreply.github.com>
2021-03-14 17:31:08 +0100

    removed icon from script

--- scripts/redirects/http-to-https/manuskript_http_to_https.user.js
+++ scripts/redirects/http-to-https/manuskript_http_to_https.user.js
@@ -2,8 +2,7 @@
 // @name         [WM] manuskript 'http' to 'https' redirect
 // @namespace    https://github.com/WidgetMidget/scripts-and-userstyles
 // @author       WidgetMidget
-// @version      1.0.0
-// @icon         https://raw.githubusercontent.com/WidgetMidget/scripts-and-userstyles/master/resources/favicons/manuskript.png
+// @version      1.0.1
 // @updateURL    https://github.com/WidgetMidget/scripts-and-userstyles/raw/master/scripts/redirects/http-to-https/manuskript_http_to_https.user.js
 // @downloadURL  https://github.com/WidgetMidget/scripts-and-userstyles/raw/master/scripts/redirects/http-to-https/manuskript_http_to_https.user.js
 // @supportURL   https://github.com/WidgetMidget/scripts-and-userstyles/issues
