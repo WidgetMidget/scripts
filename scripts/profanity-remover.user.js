29c22ba6b6b6d4a46b45226e6ec722f9e7d6716d
WidgetMidget <67565373+WidgetMidget@users.noreply.github.com>
2021-06-02 17:21:16 +0200

    Create profanity-remover.user.js

---
+++ scripts/profanity-remover.user.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name         [WM] Profanity remover
+// @namespace    https://github.com/WidgetMidget/scripts-and-userstyles
+// @author       WidgetMidget
+// @description  Removes various profanities, though the script probably isn't very stable...
+// @version      0.3.6
+// @updateURL    https://github.com/WidgetMidget/scripts-and-userstyles/raw/master/scripts/profanity-remover.user.js
+// @downloadURL  https://github.com/WidgetMidget/scripts-and-userstyles/raw/master/scripts/profanity-remover.user.js
+// @supportURL   https://github.com/WidgetMidget/scripts-and-userstyles/issues
+// @include      http://*
+// @include      https://*
+// @exclude      https://github.com/*
+// ==/UserScript==
+
+// Harvested from u/isochronism at https://www.reddit.com/r/Awesomenauts/comments/32fim8/a_profanity_filter_extension_for_your_browser/
+// Thank you 🙂
+
+function recursiveFindTextNodes(ele) {
+  var result = [];
+  result = findTextNodes(ele,result);
+  return result;
+}
+
+function findTextNodes(current,result) {
+  for(var i = 0; i < current.childNodes.length; i++) {
+    var child = current.childNodes[i];
+    if(child.nodeType == 3) {
+      result.push(child);
+    }
+    else {
+      result = findTextNodes(child,result);
+    }
+  }
+  return result;
+}
+
+var l = recursiveFindTextNodes(document.body);
+
+for(var i = 0; i < l.length; i++) {
+  var t = l[i].nodeValue;
+  t = t.replace(/fucking|motherfucker|fucker|fucked|fuck|pissed|piss/gi, "[REDACTED]");
+  t = t.replace(/shitty|shit/gi, "[REDACTED]");
+  t = t.replace(/nigger/gi, "[REDACTED]");
+  t = t.replace(/cock|dick|cunt/gi, "[REDACTED]");
+  t = t.replace(/jesusfreak|god-awful/gi, "[REDACTED]");
+  l[i].nodeValue = t;
+}
