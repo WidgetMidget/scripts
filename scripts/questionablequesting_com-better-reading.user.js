a057b67a6a56121306804152a805e345390e0ef3
frigvid <67565373+frigvid@users.noreply.github.com>
2024-03-03 12:30:32 +0100

    feat: Rename QQ and add new functionality.

---
+++ scripts/questionablequesting_com-better-reading.user.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name        [Frg] Questionable Questing better reading
+// @namespace   https://github.com/frigvid/scripts-and-userstyles
+// @author      frigvid
+// @description
+// @version     1.5.0
+// @icon        https://www.google.com/s2/favicons?domain=questionablequesting.com
+// @updateURL   https://github.com/frigvid/scripts-and-userstyles/raw/master/scripts/questionablequesting_com-better-reading.user.js
+// @downloadURL https://github.com/frigvid/scripts-and-userstyles/raw/master/scripts/questionablequesting_com-better-reading.user.js
+// @supportURL  https://github.com/frigvid/scripts-and-userstyles/issues
+// @match       https://forum.questionablequesting.com/threads/*/reader
+// ==/UserScript==
+
+// Replace asterisks with linebreaks where relevant.
+(function() {
+    'use strict';
+
+    // Regular expression to match strings of stars with a minimum length of 5 or longer
+    var starPattern = /\*{10,}/g;
+
+    // Find all instances of the star pattern and replace them with an <hr> element
+    document.body.innerHTML = document.body.innerHTML.replace(starPattern, '<hr>');
+})();
+
+// Click on spoiler tags, if there are any.
+(function() {
+    'use strict';
+
+    function revealSpoilers() {
+        document.querySelectorAll('.bbCodeSpoilerButton').forEach(function(button) {
+            button.click();
+            // Find the associated image containers within the spoiler for each button
+            button.closest('.bbCodeSpoilerContainer').querySelectorAll('.SpoilerTarget').forEach(container => {
+                // Set the display of the container to 'unset'
+                container.style.display = 'unset';
+                // Ensure images within this container have their 'src' set for loading
+                container.querySelectorAll('img.lazyload').forEach(img => {
+                    if (img.dataset.src) {
+                        img.src = img.dataset.src;
+                    }
+                });
+            });
+        });
+    }
+
+    window.addEventListener('load', revealSpoilers);
+})();
